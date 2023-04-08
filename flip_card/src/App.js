3604e614cb6edc1d26d5b343d6b2e1522e01e025
Tich Zvidzayi <tzvidzie@gmail.com>
2023-04-08 20:49:14 +0200

    init

--- flip_card/src/App.js
+++ flip_card/src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <h1> Flip cards</h1>
+       <button> New Game</button>
     </div>
   );
 }
