f94691aed8ad2fcc542da3e523c6e8ed4547483c
Tich Zvidzayi <tzvidzie@gmail.com>
2023-04-09 18:11:56 +0200

    reset

--- flip_card/src/App.js
+++ flip_card/src/App.js
@@ -1,6 +1,11 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import SingleCard from "./components/SingleCard";
+import Lottie from "lottie-react";
+import memoryanimation from "./cardmemory.json";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const tiles = [
   { src: "/img/dolphin.png", matched: false },
@@ -44,6 +49,12 @@ function App() {
     shuffle_tiles();
   }, []);
 
+
+// Reset the game.
+
+
+
+
   //Check for matching cards
 
   useEffect(
@@ -51,7 +62,7 @@ function App() {
       if (choice1 && choice2) {
         setLocked(true);
         if (choice1.src === choice2.src) {
-          console.log(choice1.src + " cards matches");
+         // console.log(choice1.src + " cards matches");
           setCards(prevTurns => {
             return prevTurns.map(card => {
               if (card.src === choice1.src) {
@@ -64,31 +75,41 @@ function App() {
 
           setTimeout(() => resetTurn(), 1200);
         } else {
-          console.log("No match");
+        //  console.log("No match");
           setTimeout(() => resetTurn(), 1200);
         }
       }
     },
     [choice1, choice2]
   );
-  console.log(cards);
+//  console.log(cards);
   const resetTurn = () => {
     setChoiceOne(null);
     setChoiceTwo(null);
     setTurns(prevTurns => prevTurns + 1);
     setLocked(false);
+
+    if(turns >= 3)
+    {
+      toast("Wow so easy!");
+      shuffle_tiles();
+     // resetGame();
+    }
   };
 
   return (
     <div className="App">
-      <h1> Flip cards</h1>
+      <div className="c_animation"><Lottie animationData={memoryanimation}/>
+      <span>
+        Turns : {turns}
+      </span>
+      </div>
+     
       <button
-        style={{ width: "100px", height: "50px" }}
         onClick={shuffle_tiles}
-      >
-        {" "}New Game
+      >New Game
       </button>
-
+      
       <div className="card_grid">
         {cards.map(card =>
           <SingleCard
@@ -100,8 +121,12 @@ function App() {
           />
         )}
       </div>
-      <p>
-        {" "}Turns :{turns}{" "}
+      
+      <p> Flip is a timed card memory game. 
+        Click the cards to see what symbol they uncover and try to 
+        find the matching symbol underneath the other cards.
+         Uncover two matching symbols at once to eliminate them from the game.
+          Eliminate all cards as fast as you can to win the game
       </p>
     </div>
   );
